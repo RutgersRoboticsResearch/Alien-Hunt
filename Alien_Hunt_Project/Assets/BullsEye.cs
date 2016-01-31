851e62174a4fe3b02f9e700e9a0fefd4b627f777
jmrisinger <Jonathan.Risinger@rutgers.edu>
2016-01-31 13:22:11 -0500

    Project file for Alien Hunt

---
+++ Alien_Hunt_Project/Assets/BullsEye.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System;
+
+using UnityStandardAssets.CrossPlatformInput;
+using UnityStandardAssets.Utility;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+public class BullsEye : MonoBehaviour {
+	// left and right marks
+	public Transform left;
+	public Transform right;
+
+
+	// speed
+	public float speed = 1.0f;
+	
+	// current direction (false means to the left, true means to the right)
+	bool dir = false;
+
+	void OnCollisionEnter(Collision c) {
+		GameObject.Find("Main Camera").GetComponent<Counts>().count ++; 
+		Destroy(gameObject);
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		if (dir) {
+			// go closer to the right one
+			transform.position = Vector3.MoveTowards(transform.position,
+			                                         right.position,
+			                                         Time.deltaTime * speed);
+			
+			// reached it?
+			if (transform.position == right.position)
+				dir = !dir; // go to opposite direction next time
+		} else {
+			// go closer to the left one
+			transform.position = Vector3.MoveTowards(transform.position,
+			                                         left.position,
+			                                         Time.deltaTime * speed);
+			
+			// reached it?
+			if (transform.position == left.position)
+				dir = !dir; // go to opposite direction next time
+		}
+	}
+}
---
+++ Alien_Hunt_Project/Assets/Counts.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Counts : MonoBehaviour {
+	public Text countText;
+	public Text winText;
+	public Text timeText;
+	public int count;
+	public float seconds, minutes, currSec, currMin;
+	// Use this for initialization
+	void Start () {
+
+		count = 0;
+
+		winText.text = "";
+
+		timeText.text = "02:00";
+	
+
+		SetCountText ();
+
+	}
+
+	void SetCountText ()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count > 14)
+		{
+			winText.text = "You Win!";
+		}
+
+		if (minutes >= 2 && count <= 14) {
+			winText.text = "Game Over";
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+		SetCountText ();
+		if (minutes <= 1) {
+			minutes = (int)(Time.time / 60f);
+			seconds = (int)(Time.time % 60f);
+			currSec = 60 - seconds;
+			currMin = 1 - minutes;
+			timeText.text = currMin.ToString ("00") + ":" + currSec.ToString ("00");
+		}
+	}
+}
